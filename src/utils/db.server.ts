8c53adaa03c977176312e7af11d15afa471398c0
nsamedaisy <nsamedaisy@gmail.com>
2024-04-25 16:11:13 +0100

    created models and defined a prisma file

---
+++ src/utils/db.server.ts
@@ -0,0 +1,17 @@
+// defining our prisma file 
+
+import { PrismaClient } from "@prisma/client";
+
+let db: PrismaClient
+
+declare global {
+    var _db: PrismaClient | undefined;
+}
+
+if (!global._db) {
+    global._db = new PrismaClient();
+}
+
+db = global._db;
+
+export { db };
